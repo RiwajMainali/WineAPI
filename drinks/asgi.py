6f07543dd49c277281dc6b3b26391a8b190a34bc
Riwaj Mainali <riwaj227@gmail.com>
2022-12-07 19:48:23 -0600

    rudimentary crud REST API

--- drinks/asgi.py
+++ drinks/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for cfehome project.
+ASGI config for drinks project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drinks.settings')
 
 application = get_asgi_application()
--- py_client/basic.py
+++
@@ -1,5 +0,0 @@
-import requests
-endpoint = "http://127.0.0.1:8000/api/"
-
-get_response=(requests.get(endpoint, params={"abc":213}, json={"query":"data"}))
-print(get_response.json())
